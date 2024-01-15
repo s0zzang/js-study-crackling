bc99e43bdd61f9eb6b3163db17935a3a1a2e40bb
s0zzang <dlth8888@gmail.com>
2024-01-15 20:45:59 +0900

    [타닥타닥 자스 스터디] 2일차 예제 준비

---
+++ day02/script.js
@@ -0,0 +1,33 @@
+// 예제 1 : 주어진 배열에서 최댓값을 찾아 반환하는 함수를 작성하세요.
+function findMax(arr) {
+
+}
+
+// 테스트
+const numbers = [3, 8, 2, 1, 5, 7, 4];
+const maxNumber = findMax(numbers);
+console.log("배열의 최댓값:", maxNumber);
+
+//////////////////////////////////////////////////////////////////////
+
+// 예제 2 : 주어진 배열의 모든 요소의 평균을 계산하여 반환하는 함수를 작성하세요.
+function calculateAverage(arr) {
+
+}
+
+// 테스트
+const scores = [85, 90, 92, 88, 78];
+const averageScore = calculateAverage(scores);
+console.log("배열 요소의 평균:", averageScore);
+
+//////////////////////////////////////////////////////////////////////
+
+// 예제 3 : 주어진 배열의 순서를 거꾸로 뒤집는 함수를 작성하세요.
+function reverseArray(arr) {
+
+}
+
+// 테스트
+const originalArray = [1, 2, 3, 4, 5];
+const reversedArray = reverseArray(originalArray);
+console.log("뒤집힌 배열:", reversedArray);
